96984518c2836fdd7cd3b2b8eabb6ef1e5ce04a9
gabito <dzangalist@gmail.com>
2024-04-22 13:41:33 +0100

    Added API gateway

---
+++ api-gateway/src/test/java/com/gabito/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabito.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
